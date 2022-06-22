58b2a82bc2e167960c1bca3a08e8fdf19be1712a
m0216i <m0216i@outlook.com>
2022-06-22 23:24:29 +0900

    #4 styled-componentsの導入

--- src/entries/app.jsx
+++ src/entries/app.jsx
@@ -2,13 +2,17 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GlobalStyle from '~/style/GlobalStyle';
 
 import App from '~/routings/App';
 
 const rootEl = document.getElementById('root');
 
 ReactDOM.render(
-	<App />,
+	<>
+		<GlobalStyle />
+		<App />
+	</>,
 	rootEl,
 );
 
