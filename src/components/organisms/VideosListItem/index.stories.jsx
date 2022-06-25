9c36bc3da4d121b960e3870d2e45164df1fbd14e
m0216i <m0216i@outlook.com>
2022-06-25 18:56:36 +0900

    #12 organismsコンポーネントの実装

---
+++ src/components/organisms/VideosListItem/index.stories.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import VideosListItem from '.';
+import video from './sampleData.json';
+
+export default { title: 'organisms/VideosListItem' };
+
+export const videoListItem = () => <VideosListItem video={video} />;
+
