919ef7c97284d11c2ac5da798660fd12ac0f7721
m0216i <m0216i@outlook.com>
2022-06-26 22:24:27 +0900

    #13 Template,Pages　コンポーネントの実装

--- src/components/pages/TopPage/index.stories.jsx
+++ src/components/pages/TopPage/index.stories.jsx
@@ -1,6 +1,39 @@
-import React from 'react';
-import TopPage from '.';
+import React, { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import VideosListTemplate from '../../templates/VideosListTemplate /index';
+import Header from '../../organisms/Header/index';
+import SearchForm from '../../organisms/SearchForm/index';
+import VideosList from '../../organisms/VideosList/index';
 
-export default { title: 'pages/TopPage' };
+export const TopPagePresenter = ({
+	search,
+	searchNext,
+	defaultKeyword,
+	videos,
+	loading,
+}) => (
+	<VideosListTemplate
+		headerContents={<Header />}
+		searchFormContents={(
+			<SearchForm onSubmit={search} defaultValue={defaultKeyword} />
+		)}
+		videosListContents={<VideosList videos={videos} loading={loading} />}
+		onScrollEnd={searchNext}
+	/>
+);
 
-export const topPage = () => <TopPage />;
+TopPagePresenter.propTypes = {
+	search: PropTypes.func.isRequired,
+	searchNext: PropTypes.func.isRequired,
+	defaultKeyword: PropTypes.string,
+	videos: VideosList.propTypes.videos,
+	loading: PropTypes.bool,
+};
+
+TopPagePresenter.defaultProps = {
+	videos: null,
+	loading: false,
+	defaultKeyword: '',
+};
+
+export default TopPagePresenter;
