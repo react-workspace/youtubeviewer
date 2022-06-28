d311510997d553ee48d2980694e4254815f4c692
m0216i <m0216i@outlook.com>
2022-06-28 21:15:22 +0900

    #24 検索機能の作成

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const express = require('express');
+
 const app = express();
 const router = express.Router();
 
 /* 3000番ポートで待ち受け */
-const server = app.listen(3000, function () {
+const server = app.listen(3000, () => {
 	console.log(`Node.js is listening to PORT: ${server.address().port}`);
 });
+
+app.use('/api', require('./api')); // 追加
 
 // 静的ファイルのルーティング
 router.use(express.static('public'));
@@ -16,3 +19,4 @@ router.get('*', (req, res, next) => {
 });
 
 app.use('/', router);
+
