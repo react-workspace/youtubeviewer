bb285284d87e7194dd691c533278059555fe695f
m0216i <m0216i@outlook.com>
2022-07-01 22:42:06 +0900

    #23 Pagesコンポーネントの実装

--- api/index.js
+++ api/index.js
@@ -11,27 +11,26 @@ const youtube = google.youtube({
 
 const router = express.Router();
 
-router.get('/videos/search/:keyword', (req, res, next) => {
-	const { keyword } = req.params;
-	const { pageToken } = req.query;
-	(async () => {
-		// 検索結果を動画IDで取得
-		const { data: { items: idItems, nextPageToken } } = await youtube.search.list({
-			part: 'id',
-			q: keyword,
-			type: 'video',
-			maxResults: 20,
-			pageToken,
-		});
-		// 動画の情報を取得
-		const ids = idItems.map(({ id: { videoId } }) => videoId);
-		const { data: { items } } = await youtube.videos.list({
-			part: 'statistics,snippet',
-			id: ids.join(','),
-		});
-		res.json({ items, nextPageToken });
-	})().catch(next);
-
+router.get('/videos/:videoId/related', (req, res, next) => {
+  const { videoId: relatedToVideoId } = req.params;
+  const { pageToken } = req.query;
+  (async () => {
+    // 関連動画のIDを取得
+    const { data: { items: idItems, nextPageToken } } = await youtube.search.list({
+      part: 'id',
+      relatedToVideoId,
+      type: 'video',
+      maxResults: 20,
+      pageToken,
+    });
+    // 動画の情報を取得
+    const ids = idItems.map(({ id: { videoId } }) => videoId);
+    const { data: { items } } = await youtube.videos.list({
+      part: 'statistics,snippet',
+      id: ids.join(','),
+    });
+    res.json({ items, nextPageToken });
+  })().catch(next);
 });
 
 module.exports = router;
